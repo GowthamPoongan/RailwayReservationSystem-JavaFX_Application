921644bc3d0dbe5b1db5d29a5888f51a7d0dab91
Gowtham P <gowthampoongan@gmail.com>
2025-05-05 19:53:05 +0530

    Update DatabaseConnection.java

--- src/main/java/com/example/railway/database/DatabaseConnection.java
+++ src/main/java/com/example/railway/database/DatabaseConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/railway";
     private static final String USER = "root";
-    private static final String PASSWORD = "Gowtham@2006"; // Replace with your password
+    private static final String PASSWORD = "you local host Password"; // Replace with your password
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
